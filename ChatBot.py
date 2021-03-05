774c98772ee08c34f43f2a7ea2697e3b20cab64a
Ta-mmih <78894159+Ta-mmih@users.noreply.github.com>
2021-03-05 10:42:49 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -237,10 +237,7 @@ if __name__ == '__main__':
                           url_path=access_token)
     updater.bot.setWebhook('https://anonmat-bot.herokuapp.com/' + access_token)
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+   
     recovery_data()
     bot.stop_polling()
     bot.polling(none_stop=True)
