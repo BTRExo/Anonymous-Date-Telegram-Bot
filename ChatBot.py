5d84ac4372061453a2cec453ec280cf05dd7fbdd
Ta-mmih <78894159+Ta-mmih@users.noreply.github.com>
2021-03-05 11:03:06 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -223,6 +223,8 @@ def echo(call):
 def main():
     updater = Updater(access_token, use_context=True)
     dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=access_token)
